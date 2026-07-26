cb28a409128ea44ae6b7c6bda925f707bcc13636
ONKAR SAWANT <169951107+Buildwithtiger@users.noreply.github.com>
2026-07-26 22:59:13 +0530

    Add files via upload

---
+++ modulesecound.mjs
@@ -0,0 +1,7 @@
+export function simple(){
+    console.log("this is secound module")
+}
+export default function simple2(){
+    console.log("this is secound module is complex")
+}
+// module.exports=simple;
