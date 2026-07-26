cf01c6233cf646572b28537c313e61a1c5d12d60
ONKAR SAWANT <169951107+Buildwithtiger@users.noreply.github.com>
2026-07-26 22:50:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,146 @@
+// // //  //way to print a java script
+// // // console.log("hellow woeld");
+// // // alert("congratulation your 1st program is run");
+// // // document.write("this is a document to write");
+
+// // const { createElement } = require("react");
+
+// // // //javascript console API
+// // // console.log(" hellow word", 77+12, "this is my money");
+// // // console.warn("alert tag");
+// // // console.error("this is error tag");
+// // // //primitive datatypes
+// // // //numbers
+// // var num1=11;
+// // var num2=44;
+// // console.log(num1+num2);
+
+// // // //string
+// // var str1="hellow bwb";
+// // var str2='hii i am tiger'
+// // console.log(str1);
+// // console.log(str2);
+
+// // // // boolean
+// // // var a=true;
+// // // var b=false;
+// // // console.log=(a);
+// // //undefined data types
+// // var und;
+// // var nodata=undefined;
+
+// // //null
+// // var n=null;
+// // console.log(n);
+
+// // //array
+// // // var arr=[11,22,33,"mj",66];
+// // // console.log(arr);
+// // // console.log(arr[0]);
+// // // console.log(arr[1]);
+// // // console.log(arr[2]);
+// // // console.log(arr[3]);
+// // // console.log(arr[4]);
+
+// // // //object
+// // // var marks={ravi:11,
+// // //     shubhan:44,
+// // //     mahesh:99
+// // // };
+// // // console.log(marks);
+
+
+// // // //function
+// // // function avg(a,b){
+// // //     return (a+b)/2;
+// // // }
+
+
+// // // c1= avg(22,33);
+// // // c2= avg(33,77);
+// // // console.log(c1, c2);
+
+
+// // // var n=16;
+// // // if(n>18){
+// // //     console.log("you are elegibel for vote")
+// // // }
+// // // else{
+// // //     console.log("you are not elegible for vote ")
+// // // }
+
+// // // var n=22;
+// // // if(18>n){
+// // //     console.log("june")
+// // // }
+// // // else if(20<n){
+// // //     console.log("may ")
+// // // }
+// // // else if(n>22){
+// // //     console.log("april ")
+// // // }
+// // // else{
+// // //     console.log("hellow word")
+// // // }
+// // // //traversing array
+// // // var arrayy=[11,22,33,44,55,66,77,88,99];
+// // // for(var i=0;i<arrayy.length; i++){
+// // //     console.log(arrayy[i]);
+// // // }
+
+
+// // // let j=0;
+// // // while(j < arrayy.length){
+// // //     console.log(arrayy[j]);
+// // //     j++;
+// // // }
+
+// // // let  mydate= new Date();
+// // // console.log(mydate);
+// // // console.log(mydate.getDay());
+// // // console.log(mydate.getFullYear());
+// // // console.log(mydate.getTime());
+
+// // // //DOM METHODS
+// // // let Elemenid =document.getElementById("click");
+// // // console.log(Elemenid);
+
+// // // let elementclass=document.getElementsByClassName("container")
+// // // console.log(elementclass);
+// // // // elementclass[0].style.background="yellow";
+// // // elementclass[0].classList.add("primary");
+// // // elementclass[0].classList.add("text");
+// // // elementclass[0].innerHTML;
+// // // // console.log(elementclass[0].innerHTML);
+// // // // console.log(elementclass[0].innerText);
+// // // // tn = document.getElementsByTagName('button');
+// // // // console.log(tn);
+// // // createdelement=document.createElement('p');
+// // // createElement.innerText="hi i am created by usinh js";
+// // // elementclass[0].appenChild(createdElement);
+
+// // function clicked(){
+// //     console.log("this btn was clicked")
+// // }
+// // window.onload = function(){
+// // console.log("load sucessfully")
+// // }
+// // firstcontainer=addEventListener('click',function(){
+// //     console.log("you clicked on container")
+// // })
+// // firstcontainer=addEventListener('mouseover',function(){
+// //     console.log("your mouse over the  container")
+// // })
+
+// // logkaro=()=>{
+// //     document.querySelector(".container")
+// //     console.log("i am your log")
+// // }
+// // setTimeout(logkaro, 2000);
+// // // setInterval(logkaro, 2000);
+
+// ??local Storage
+localStorage.setItem("name","bossss");//to store iten in local storge
+localStorage.getItem("name");// to get item from local storage
+localStorage.clear;//to clear data from local storage
+localStorage //to seee all type of data
